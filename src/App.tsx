614f0024577505c8f199b6d3bad64fe3528b3d37
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-06-05 21:41:54 +0900

    update frontend

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import axios from 'axios'
 function App() {
   useEffect(() => {
     const getTask = async () => {
-      const res = await axios.get<Task>(`${process.env.REACT_APP_API_URL}/api/todo`)
+      const res = await axios.get<Task>(`${process.env.REACT_APP_API_URL}/todo`)
       console.log(res.data);
     }
     getTask();
