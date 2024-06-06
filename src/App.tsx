e02d0c753304e86a9fea8a7d3120dda5723f504e
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-06-06 21:26:37 +0900

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { Task } from './types/types'
 import axios from 'axios'
 
 function App() {
+  const [tasks, setTasks] = useState<Task[]>([])
   useEffect(() => {
     const getTask = async () => {
-      const res = await axios.get<Task>(`${process.env.REACT_APP_API_URL}/todo`)
-      console.log(res.data);
+      const res = await axios.get(`${process.env.REACT_APP_API_URL}/todo`)
+      setTasks(res.data)
+      console.log(res.data)
     }
-    getTask();
-  },[])
-  return <div></div>
+    getTask()
+  }, [])
+  return (
+    <>
+      {tasks.map((task) => (
+        <div key={task.id}>
+          <p>{task.title}</p>
+          <p>{task.description}</p>
+        </div>
+      ))}
+    </>
+  )
 }
 
 export default App
