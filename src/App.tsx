ce2b2889afecb6ccc836cef87378c6c7ca42dad5
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-06-07 18:36:55 +0900

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { useEffect, useState } from 'react'
 import { Task } from './types/types'
 import axios from 'axios'
+import { Container, Paper } from '@mui/material'
 
 function App() {
+  const paperStyle = { padding: "30px 20px", maxWidth: 600, margin: "50px auto", }
+  // const [title, setTitle] = useState('')
+  // const [description, setDescription] = useState('')
   const [tasks, setTasks] = useState<Task[]>([])
   useEffect(() => {
     const getTask = async () => {
@@ -11,15 +15,21 @@ function App() {
       console.log(res.data)
     }
     getTask()
-  }, [])
+  }, [tasks])
   return (
     <>
-      {tasks.map((task) => (
-        <div key={task.id}>
-          <p>{task.title}</p>
-          <p>{task.description}</p>
-        </div>
-      ))}
+      <Container>
+          <Paper elevation={3} style={paperStyle}>
+              <h2 style={{ color: "#1976d2" }}>Tasks</h2>
+              {tasks.map((task,index) => (
+                  <Paper elevation={3} style={{margin:"10px",padding:"15px",textAlign:"left", display: 'flex', justifyContent: 'space-between', alignItems: 'center'}} key={index}>
+                      <div>{task.title} : {task.description}</div>
+                      <div>
+                      </div>
+                  </Paper>
+              ))}
+          </Paper>
+      </Container>
     </>
   )
 }
