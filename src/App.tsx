35395c5138a1984d04a0095b2eea30552d1573ae
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-06-08 19:27:44 +0900

    add header & button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from 'react'
 import { Task } from './types/types'
 import axios from 'axios'
-import { Container, Paper } from '@mui/material'
+import { Box, Button, Container, Paper } from '@mui/material'
+import { blue } from '@mui/material/colors'
+import Header from './components/Header'
 
 function App() {
-  const paperStyle = { padding: "30px 20px", maxWidth: 600, margin: "50px auto", }
+  const paperStyle = {
+    padding: '30px 20px',
+    maxWidth: 600,
+    margin: '50px auto',
+  }
   // const [title, setTitle] = useState('')
   // const [description, setDescription] = useState('')
   const [tasks, setTasks] = useState<Task[]>([])
@@ -18,17 +24,42 @@ function App() {
   }, [tasks])
   return (
     <>
+      <Header />
       <Container>
-          <Paper elevation={3} style={paperStyle}>
-              <h2 style={{ color: "#1976d2" }}>Tasks</h2>
-              {tasks.map((task,index) => (
-                  <Paper elevation={3} style={{margin:"10px",padding:"15px",textAlign:"left", display: 'flex', justifyContent: 'space-between', alignItems: 'center'}} key={index}>
-                      <div>{task.title} : {task.description}</div>
-                      <div>
-                      </div>
-                  </Paper>
-              ))}
-          </Paper>
+        <Paper elevation={2} style={paperStyle}>
+          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            <h2 style={{ color: '#1976d2' }}>Tasks</h2>
+          </Box>
+          {tasks.map((task, index) => (
+            <Box
+              key={index}
+              sx={{
+                padding: '15px',
+                textAlign: 'left',
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                backgroundColor: index % 2 === 0 ? blue[50] : 'white',
+              }}
+            >
+              <div>
+                {task.title} : {task.description}
+              </div>
+              <div>
+                <Button variant="contained" sx={{ margin: '0 5px' }}>
+                  Edit
+                </Button>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  sx={{ margin: '0 5px' }}
+                >
+                  Delete
+                </Button>
+              </div>
+            </Box>
+          ))}
+        </Paper>
       </Container>
     </>
   )
