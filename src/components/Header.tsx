16aeb34ef59fd414908d378eea307323cf085676
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-06-08 19:27:22 +0900

    add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+
+export default function Header() {
+  return (
+    <AppBar position="static">
+      <Toolbar sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Typography variant="h6" component="div">
+          <h1 style={{ fontSize: '22px' }}>React FastAPI Todo App</h1>
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  )
+}
