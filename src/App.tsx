1d9b068921bbb60bd0610f5bd298873f45768320
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-06-10 18:08:56 +0900

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { BrowserRouter } from 'react-router-dom'
 import MyRouter from './router/index'
 import Header from './components/Header'
-import { grey } from '@mui/material/colors'
 import { Box } from '@mui/material'
 
 function App() {
   return (
     <BrowserRouter>
-      <Box sx={{ backgroundColor: grey[100], minHeight: '100vh' }}>
+      <Box>
         <Header />
         <MyRouter />
       </Box>
