16005b3cd98ddd18d0563aeac96ce33cd71437a2
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-06-14 18:45:51 +0900

    delete react-query

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,15 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { store } from './app/store'
-import App from './App'
 import reportWebVitals from './reportWebVitals'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import{ ReactQueryDevtools } from 'react-query/devtools'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: false,
-      refetchOnWindowFocus: false,
-    }
-  }
-})
-
-const container = document.getElementById('root')!;
-const root = createRoot(container);
-
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <QueryClientProvider client={queryClient}>
-    <Provider store={store}>
-      <App />
-    </Provider>
-    <ReactQueryDevtools initialIsOpen={false} />
-  </QueryClientProvider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
