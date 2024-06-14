ee11dcc1af13c7089b2efffd62fced5f0759f3bf
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-06-14 18:46:02 +0900

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { BrowserRouter } from 'react-router-dom'
 import MyRouter from './router/index'
 import Header from './components/Header'
-import { Box } from '@mui/material'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Box>
-        <Header />
-        <MyRouter />
-      </Box>
-    </BrowserRouter>
+    <div className="App">
+      <Header />
+      <MyRouter />
+    </div>
   )
 }
 
