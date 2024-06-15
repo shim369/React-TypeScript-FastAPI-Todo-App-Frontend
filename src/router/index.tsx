4737e4a57c2f96b8087f17cefeb36e56b07cbe0a
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-06-15 19:33:04 +0900

    update router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom"
 import Task from "../pages/Task"
+import TaskCreate from "../pages/TaskCreate"
 import TaskEdit from "../pages/TaskEdit"
 
 function MyRouter() {
   return (
     <Routes>
         <Route path="/" element={<Task />} />
+        <Route path="/tasks/create" element={<TaskCreate />} />
         <Route path="/tasks/:id/edit" element={<TaskEdit />} />
     </Routes>
   );
